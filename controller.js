072fc44f417ef69aa1a0ef30803e3b0f1528e295
DizzlyD <david4henery00@hotmail.com>
2022-03-17 14:38:37 +0000

    chart modules

--- controller.js
+++ controller.js
@@ -12,72 +12,49 @@ const soccer = require("soccer.js").soccer;
 console.log("works");
 // console.log(TeamView);
 
-// WILL MOVE TO SEPERATE MODULE (ALL CHARTS)
-// const data = {
-//   labels: ["Wins", "Draws", "Loses"],
+
+
+// // line chart
+// const dataLineChart = {
+//   labels: [
+//     "Aug",
+//     "Sep",
+//     "Oct",
+//     "Nov",
+//     "Dec",
+//     "Jan",
+//     "Feb",
+//     "Mar",
+//     "April",
+//     "May",
+//   ],
 //   datasets: [
 //     {
-//       label: "My First Dataset",
-//       data: [22, 4, 7],
-//       backgroundColor: [
-//         "rgb(34, 169, 227)",
-//         "rgba(34, 169, 227, 0.25)",
-//         "rgb(200, 205, 86)",
-//       ],
-//       hoverOffset: 4,
+//       label: "League Position over the season",
+//       data: [20, 5, 10, 15, 10, 1, 2, 6, 3],
+//       fill: false,
+//       borderColor: "#22A9E3",
+//       tension: 0.1,
 //     },
 //   ],
 // };
 
-// const config = {
-//   type: "doughnut",
-//   data: data,
+// const configLineChart = {
+//   type: "line",
+//   data: dataLineChart,
+//   options: {
+//     scales: {
+//       y: {
+//         reverse: true,
+//         max: 20,
+//         min: 1,
+//       },
+//     },
+//   },
 // };
-// const ctx = document.getElementById("myChart");
-// const myChart = new Chart(ctx, config);
 
-// line chart
-const dataLineChart = {
-  labels: [
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-    "Jan",
-    "Feb",
-    "Mar",
-    "April",
-    "May",
-  ],
-  // labels: ["hello", "gre"],
-  datasets: [
-    {
-      label: "League Position over the season",
-      data: [20, 5, 10, 15, 10, 1, 2, 6, 3],
-      fill: false,
-      borderColor: "#22A9E3",
-      tension: 0.1,
-    },
-  ],
-};
-
-const configLineChart = {
-  type: "line",
-  data: dataLineChart,
-  options: {
-    scales: {
-      y: {
-        reverse: true,
-        max: 20,
-        min: 1,
-      },
-    },
-  },
-};
-
-const lineElement = document.getElementById("line-chart");
-const myLineChart = new Chart(lineElement, configLineChart);
+// const lineElement = document.getElementById("line-chart");
+// const myLineChart = new Chart(lineElement, configLineChart);
 
 // WILL ALSO MOVE TO OTHER MODULE!
 const on = soccer.addTeam(["Arsenal", "Aston Villa","Burnley","Brentford","Brighton", "Cystal Palace", "Chelsea", "Everton","Leicester","Leeds", "Liverpool", "Man United", "Man City", "Norwich", "Newcastle","Southampton", "Tottenham","Watford", "Wolves", "West Ham",])
