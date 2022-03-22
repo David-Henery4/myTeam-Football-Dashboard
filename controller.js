7ddaa783b6bcb6fde645ae98c28746e06c5e64c6
DizzlyD <david4henery00@hotmail.com>
2022-03-22 19:49:41 +0000

    UIs

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ import * as model from "./model.js";
 import TeamView from "./views/teamView.js";
 import PieView from "./views/pieView.js";
 import PlayersView from "./views/playersView.js";
-import FormView from "./views/formView.js";
+// import FormView from "./views/formView.js";
 import LeagueView from "./views/leagueView.js";
 import teamNameView from "./views/teamNameView.js";
 import Prediction from "./views/predictionView.js"
